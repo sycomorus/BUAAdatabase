870c00e3b7a72af2de70d7704278e13e40a471eb
周泽同 <3306921258@buaa.edu.cn>
2024-10-15 23:58:41 +0800

    这是一个斜杠的故事

--- frontend/src/mock/user/login.js
+++ frontend/src/mock/user/login.js
@@ -1,36 +1,35 @@
-import Mock from 'mockjs'
-import '@/mock/extend'
+import Mock from "mockjs";
+import "@/mock/extend";
 
-Mock.mock(`${process.env.VUE_APP_API_BASE_URL}/login`, 'post', ({body}) => {
-  let result = {data: {}}
-  const {name, password} = JSON.parse(body)
+Mock.mock(`${process.env.VUE_APP_API_BASE_URL}/login`, "post", ({ body }) => {
+  let result = { data: {} };
+  const { name, password } = JSON.parse(body);
 
-  let success = false
+  let success = false;
 
-  if (name === 'admin' && password === '888888') {
-    success = true
-    result.data.roles = [{id: 'admin'}]
-    result.data.id = '0'
-  } else if (name === 'teacher' && password === '888888') {
-    success = true
-    result.data.roles = [{id: 'teacher'}]
-    result.data.id = '1'
-  } else if (name == 'student' && password == '888888') {
-    success = true
-    result.data.roles = [{id: 'student'}]
-    result.data.id = '2'
-  }
-  else {
-    success = false
+  if (name === "admin" && password === "888888") {
+    success = true;
+    result.data.roles = [{ id: "admin" }];
+    result.data.id = "0";
+  } else if (name === "teacher" && password === "888888") {
+    success = true;
+    result.data.roles = [{ id: "teacher" }];
+    result.data.id = "1";
+  } else if (name == "student" && password == "888888") {
+    success = true;
+    result.data.roles = [{ id: "student" }];
+    result.data.id = "2";
+  } else {
+    success = false;
   }
 
   if (success) {
-    result.code = 0
-    result.data.token = 'Authorization:' + Math.random()
-    result.message = '登录成功'
+    result.code = 0;
+    result.data.token = "Authorization:" + Math.random();
+    result.message = "登录成功";
   } else {
-    result.code = -1
-    result.message = '账户名或密码错误（admin/888888 or test/888888）'
+    result.code = -1;
+    result.message = "账户名或密码错误（admin/888888 or test/888888）";
   }
-  return result
-})
+  return result;
+});
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,13 +1,13 @@
 //跨域代理前缀
 // const API_PROXY_PREFIX='/api'
 // const BASE_URL = process.env.NODE_ENV === 'production' ? process.env.VUE_APP_API_BASE_URL : API_PROXY_PREFIX
-const BASE_URL = process.env.VUE_APP_API_BASE_URL
+const BASE_URL = process.env.VUE_APP_API_BASE_URL;
 module.exports = {
-  LOGIN: `${BASE_URL}/login`,
-  ROUTES: `${BASE_URL}/routes`,
-  REGISTER: `${BASE_URL}/register`,
-  ACTIVITIES: `${BASE_URL}/activities`,
-  SEND_POST: `${BASE_URL}/sendPost`,
-  SAVE_POST: `${BASE_URL}/savePost`,
-  GET_SAVED_POST: `${BASE_URL}/getSavedPost`,
-}
+  LOGIN: `${BASE_URL}/login/`,
+  ROUTES: `${BASE_URL}/routes/`,
+  REGISTER: `${BASE_URL}/register/`,
+  ACTIVITIES: `${BASE_URL}/activities/`,
+  SEND_POST: `${BASE_URL}/sendPost/`,
+  SAVE_POST: `${BASE_URL}/savePost/`,
+  GET_SAVED_POST: `${BASE_URL}/getSavedPost/`,
+};
