60452d6b3d8380c0c72ad8a299ce0dc77ea97c2a
张鑫 <22373090@buaa.edu.cn>
2024-12-04 20:31:10 +0800

    更改logo颜色

--- frontend/src/pages/login/Login.vue
+++ frontend/src/pages/login/Login.vue
@@ -2,7 +2,7 @@
   <common-layout>
     <div class="top">
       <div class="header">
-        <img alt="logo" class="logo" src="@/assets/img/logo.png" />
+        <img alt="logo" class="logo" src="@/assets/img/logo_black.png" />
         <span class="title">{{ systemName }}</span>
       </div>
       <div class="desc">一对一上门家教</div>
