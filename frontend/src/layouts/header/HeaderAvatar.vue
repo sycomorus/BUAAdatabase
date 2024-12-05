5290bf578eaf80a68e825177738a4327ad6bbc80
张鑫 <22373090@buaa.edu.cn>
2024-12-05 16:27:07 +0800

    实现重置密码前端部分

--- frontend/src/layouts/header/HeaderAvatar.vue
+++ frontend/src/layouts/header/HeaderAvatar.vue
@@ -1,29 +1,51 @@
 <template>
-  <a-dropdown>
-    <div class="header-avatar" style="cursor: pointer">
-      <span class="name">{{ user.name }}</span>
-    </div>
-    <a-menu :class="['avatar-menu']" slot="overlay">
-      <a-menu-item @click="toPersonalPage">
-        <a-icon type="user" />
-        <span>个人主页</span>
-      </a-menu-item>
-      <a-menu-item @click="toEditPersonalPage">
-        <a-icon type="edit" />
-        <span>编辑个人主页</span>
-      </a-menu-item>
-      <a-menu-divider />
-      <a-menu-item @click="logout">
-        <a-icon style="margin-right: 8px;" type="poweroff" />
-        <span>退出登录</span>
-      </a-menu-item>
-    </a-menu>
-  </a-dropdown>
+  <div>
+    <a-dropdown>
+      <div class="header-avatar" style="cursor: pointer">
+        <span class="name">{{ user.name }}</span>
+      </div>
+      <a-menu :class="['avatar-menu']" slot="overlay">
+        <a-menu-item @click="toPersonalPage">
+          <a-icon type="user" />
+          <span>个人主页</span>
+        </a-menu-item>
+        <a-menu-item @click="toEditPersonalPage">
+          <a-icon type="edit" />
+          <span>编辑个人主页</span>
+        </a-menu-item>
+        <a-menu-divider />
+        <a-menu-item @click="toResetPassword">
+          <a-icon type="lock" />
+          <span>修改密码</span>
+        </a-menu-item>
+        <a-menu-item @click="logout">
+          <a-icon style="margin-right: 8px;" type="poweroff" />
+          <span>退出登录</span>
+        </a-menu-item>
+      </a-menu>
+    </a-dropdown>
+    <a-modal title="修改密码" :visible="isModalVisible" @ok="handleOk" @cancel="handleCancel">
+      <a-form :form="passwordForm">
+          <a-form-item :label="'旧密码'" :labelCol="{ span: 7 }" :wrapperCol="{ span: 10 }" :required=false>
+            <a-input :placeholder="'请输入旧密码'" type="password"
+              v-decorator="['oldPassword', { rules: [{ required: true, message: '请输入旧密码', whitespace: true }], validateTrigger: 'onSubmit' }]" />
+          </a-form-item>
+          <a-form-item :label="'新密码'" :labelCol="{ span: 7 }" :wrapperCol="{ span: 10 }" :required=false>
+            <a-input :placeholder="'请输入新密码'" type="password"
+              v-decorator="['newPassword', { rules: [{ required: true, message: '请输入新密码', whitespace: true }], validateTrigger: 'onSubmit' }]" />
+          </a-form-item>
+          <a-form-item :label="'确认密码'" :labelCol="{ span: 7 }" :wrapperCol="{ span: 10 }" :required=false>
+            <a-input :placeholder="'请确认密码'" type="password"
+              v-decorator="['confirmedPassword', { rules: [{ required: true, message: '请确认密码', whitespace: true }], validateTrigger: 'onSubmit' }]" />
+          </a-form-item>
+        </a-form>
+    </a-modal>
+  </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-import { logout } from '@/services/user'
+import { logout,resetPassword } from '@/services/user'
 import { mapState } from 'vuex'
 
 export default {
@@ -31,6 +53,13 @@ export default {
   computed: {
     ...mapGetters('account', ['user']),
     ...mapState('account', { currRoles: 'roles' }),
+    ...mapState('account', { currUser: 'user' }),
+  },
+  data() {
+    return {
+      isModalVisible: false, // 控制 a-modal 显示状态的变量
+      passwordForm: this.$form.createForm(this),
+    };
   },
   methods: {
     logout() {
@@ -51,7 +80,37 @@ export default {
       } else if (this.currRoles[0].id === 'teacher') {
         this.$router.push({ name: '编辑家教主页', params: { id: this.user.id } })
       }
-    }
+    },
+    toResetPassword() {
+      this.isModalVisible = true; // 显示 a-modal
+    },
+    handleOk() {
+      this.passwordForm.validateFields((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values);
+          const { oldPassword, newPassword, confirmedPassword } = values;
+          if (newPassword !== confirmedPassword) {
+            this.$message.error('两次输入的密码不一致');
+          } else {
+            resetPassword(this.currUser.id, oldPassword, newPassword).then(res => {
+              const data = res.data;
+              if (data.code === 0) {
+                this.$message.success('修改密码成功');
+                this.isModalVisible = false; // 隐藏 a-modal
+                logout();
+                this.$router.push('/login')
+              } else {
+                this.$message.error(data.message);
+              }
+            });
+          }
+        }
+      });
+    },
+    handleCancel() {
+      this.isModalVisible = false; // 隐藏 a-modal
+    },
+
   }
 }
 </script>
